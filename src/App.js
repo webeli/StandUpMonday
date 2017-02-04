62b0b1659b0890176a80e9cadb6333cfb7fe88c9
Elias Hansen <pe_lias@msn.com>
2017-02-04 18:39:26 +0100

    Changed team to room in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Router, Route, browserHistory } from 'react-router';
 
 import Start from './Start/Start';
-import Team from './Team/Team';
+import Room from './Room/Room';
 import Profile from './Profile/Profile';
 
 class App extends Component {
@@ -10,7 +10,7 @@ class App extends Component {
     return (
       <Router history={browserHistory}>
         <Route path='/' component={Start}/>
-        <Route path='team/:id' component={Team}/>
+        <Route path='room/:id' component={Room}/>
         <Route path='profile' component={Profile}/>
       </Router>
     );
