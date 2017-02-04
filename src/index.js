c749396903ad9b4bc8da91bcd5967dc00ad9d297
Elias Hansen <pe_lias@msn.com>
2017-02-04 23:29:27 +0100

    Implemented provider with stores

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import Store from './Store';
+import GlobalStore from './Global.Store';
+import RoomStore from './Room/Room.Store';
+import StartStore from './Start/Start.Store';
 import { Provider } from 'mobx-react';
 
+const stores = {
+  globalStore: GlobalStore,
+  startStore: StartStore,
+  roomStore: RoomStore
+};
+
 ReactDOM.render(
-  <Provider>
+  <Provider {...stores}>
     <App />
   </Provider>,
   document.getElementById('root')
