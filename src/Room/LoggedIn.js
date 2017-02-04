f1e0fc584715362a463063ecddc651e4bb20e707
Salmin <salmin89@hotmail.com>
2017-02-05 00:45:58 +0100

    Added checkRoom and addToAttendees

--- src/Room/LoggedIn.js
+++ src/Room/LoggedIn.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
+import { observer, inject } from 'mobx-react';
 
+const styles = {
+  standingButton: {
+    height: 100,
+    width: 100
+  },
+  row: {
+    display: 'flex',
+    justifyContent: 'space-around'
+  },
+}
+
+@inject('globalStore', 'roomStore')
+@observer
 class LoggedIn extends Component {
+
+  constructor(props) {
+    super(props);
+    props.roomStore.addToAttendees(this.props.globalStore.user);
+  }
+
+  setSelfToStand() {
+    this.props.roomStore.setSelfToStand();
+  }
+
   render() {
-    return (
-      <div>
-        LoggedIn
+    return this.props.roomStore.roomFound && (
+      <div style={styles.row}>
+        <div>
+          <ul>
+            <li>sa</li>
+          </ul>
+        </div>
+        
+        <div>
+          <div>
+            00:00
+          </div>
+          <button style={styles.standingButton} onClick={() => this.setSelfToStand()}>I'm STANDING!</button>
+        </div>
+
+        <div>
+          asd
+        </div>
       </div>
     );
   }
--- src/Room/Room.Store.js
+++ src/Room/Room.Store.js
@@ -1,15 +1,31 @@
 import { observable } from 'mobx';
-// import { dbRoom } from '../Common/WebAPIUtils';
-// import { browserHistory } from 'react-router';
+import { dbRoom } from '../Common/WebAPIUtils';
+import { browserHistory } from 'react-router';
 
 class RoomStore {
   @observable roomName = '';
+  @observable roomFound = false;
   @observable attendees = [];
 
+  checkRoom(roomName) {
+    dbRoom.child(roomName).once('value', (snap) => {
+        if (snap.exists()) {
+          this.roomName = roomName;
+          this.roomFound = true;
+        } else {
+          browserHistory.push(`/`);
+        }
+    });
+  }
 
-  loadRoom(roomName) {
-    //is user logged in?
-    //console.log("a", roomName);
+  addToAttendees(user) {
+    const myUser = {
+      displayName: user.displayName ? user.displayName : user.email,
+      //date: Date.now(),
+      isStanding: false
+    }
+
+    dbRoom.child(this.roomName).child("attendees").child(user.uid).set(myUser);
   }
 }
 
