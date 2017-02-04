a3615ee8b4c88941fb1f2fec8248bea9ceb38377
Elias Hansen <pe_lias@msn.com>
2017-02-05 00:14:08 +0100

    Styling on Start

--- src/Start/Start.js
+++ src/Start/Start.js
@@ -1,44 +1,68 @@
 import React, { Component } from 'react';
-import { observer, inject } from 'mobx-react';
+import { observer } from 'mobx-react';
+import StartStore from './Start.Store';
 
-@inject ('globalStore', 'startStore')
+const styles = {
+  row: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  input: {
+    padding: 5,
+    borderRadius: 5,
+    border: '1px solid #CCC',
+    marginRight: 5,
+  },
+  button: {
+    padding: 5,
+    borderRadius: 5,
+    background: '#FFF',
+    border: '1px solid #CCC',
+  }
+}
+
 @observer
 export default class Start extends Component {
   findRoom(e) {
     e.preventDefault();
-    this.props.startStore.roomExists(this.props.startStore.roomName);
+    StartStore.roomExists(StartStore.roomName);
   }
   createRoom(e) {
-    if (e) { e.preventDefault(); }
-    this.props.startStore.createRoom(this.props.startStore.roomName);
+    if (e) {
+      e.preventDefault();
+    }
+    StartStore.createRoom(StartStore.roomName);
   }
   handleChangeInput(e) {
-    this.props.startStore.roomName = e.target.value;
-    this.props.startStore.notFoundMessage = false;
+    StartStore.roomName = e.target.value;
+    StartStore.notFoundMessage = false;
   }
   render() {
     return (
-      <div>
+      <div style={styles.row}>
         <h1>Enter Room</h1>
         <form onSubmit={(e) => this.findRoom(e)}>
           <input
-            value={this.props.startStore.roomName}
+            style={styles.input}
+            value={StartStore.roomName}
             onChange={(e) => this.handleChangeInput(e)}
             type="text"
             required />
-          <button type="submit">Enter</button>
+          <button style={styles.button} type="submit">Enter</button>
         </form>
-        {this.props.startStore.notFoundMessage &&
-          <p>This room does not exist <button onClick={() => this.createRoom()}>Create</button></p>}
+        {StartStore.notFoundMessage &&
+          <p>This room does not exist <button style={styles.button} onClick={() => this.createRoom()}>Create</button></p>}
         <h1>Create Room</h1>
         <form onSubmit={(e) => this.createRoom(e)}>
           <input
-            value={this.props.startStore.roomName}
+            style={styles.input}
+            value={StartStore.roomName}
             onChange={(e) => this.handleChangeInput(e)}
             className="newRoomText"
             type="text"
             required />
-          <button type="submit">Create</button>
+          <button style={styles.button} type="submit">Create</button>
         </form>
       </div>
     );
