9a1ce1761864d71d8574b235bad223056d8a9e94
Elias Hansen <pe_lias@msn.com>
2017-02-04 23:31:20 +0100

    Progress on Room component

--- src/Room/Room.js
+++ src/Room/Room.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import RoomStore from './Room.Store';
+import { observer, inject } from 'mobx-react';
 
+import LoggedIn from './LoggedIn';
+import NotLoggedIn from './NotLoggedIn';
+
+@inject ('globalStore', 'roomStore')
+@observer
 class Room extends Component {
   constructor(props) {
     super(props);
-    console.log(props);
-    RoomStore.loadRoom(props.params.id);
+    props.globalStore.checkAuth();
   }
   
   render() {
-    return (
-      <div>Room</div>
+    return this.props.globalStore.loggedIn !== null && (
+      <div>
+        { this.props.globalStore.loggedIn ? <LoggedIn /> : <NotLoggedIn /> }
+      </div>
     );
   }
 }
