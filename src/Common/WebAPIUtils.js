7613ea559008904d031e4cd3c0a213f8456569c0
Elias Hansen <pe_lias@msn.com>
2017-02-04 18:39:00 +0100

    Implemented firebase

---
+++ src/Common/WebAPIUtils.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyCr_ugwfiUTMRNo4q85UxiQCe1Vw83-j2o",
+    authDomain: "standupmonday.firebaseapp.com",
+    databaseURL: "https://standupmonday.firebaseio.com",
+    storageBucket: "standupmonday.appspot.com",
+    messagingSenderId: "1079785500807"
+};
+firebase.initializeApp(config);
+
+const dbRoom = firebase.database().ref('room');
+
+const fbAuth = firebase.auth();
+
+export { dbRoom, fbAuth }
