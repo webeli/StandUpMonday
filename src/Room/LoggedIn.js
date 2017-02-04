a3d91d084cb0ef05b01c53155ecc293dbe51aeeb
Elias Hansen <pe_lias@msn.com>
2017-02-04 23:29:59 +0100

    Created Login component

---
+++ src/Room/LoggedIn.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoggedIn extends Component {
+  render() {
+    return (
+      <div>
+        LoggedIn
+      </div>
+    );
+  }
+}
+
+export default LoggedIn;
