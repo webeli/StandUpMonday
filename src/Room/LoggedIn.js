01ece6d0f92a568e449a27cfd4247de9bd757a5d
Salmin <salmin89@hotmail.com>
2017-02-05 22:09:01 +0100

    new temporary layout

--- src/Room/LoggedIn.js
+++ src/Room/LoggedIn.js
@@ -2,28 +2,8 @@ import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
 import { getToday } from './Room.Store';
 
-import { Button, Badge } from 'antd';
-
-
-const styles = {
-  row: {
-    display: 'flex',
-    justifyContent: 'space-around',
-    marginTop: '15px'
-  },
-  input: {
-    padding: 5,
-    borderRadius: 5,
-    border: '1px solid #CCC',
-    marginRight: 5,
-  },
-  button: {
-    padding: 15,
-    borderRadius: 5,
-    background: '#FFF',
-    border: '1px solid #CCC',
-  }
-}
+import { Button, Badge, Row, Col, Layout } from 'antd';
+const { Header, Footer, Content } = Layout;
 
 @inject('globalStore', 'roomStore', 'routing')
 @observer
@@ -51,40 +31,41 @@ class LoggedIn extends Component {
     const mappedAttendees = attendees && Object.keys(attendees).map((key) => {
 
       const isStanding = (attendees[key].standingDate === getToday() && attendees[key].sittingDate !== getToday());
-      total = isStanding ? total+1 : total;
+      total = isStanding ? total + 1 : total;
 
       return (
-        <li key={key}>
+        <Col span={14} offset={10} key={key} style={{textAlign: 'left'}}>
           <Badge status={isStanding ? "success" : "error"} />
           <span className="head-example">{attendees[key].displayName}</span>
-        </li>
+        </Col>
       );
     });
 
     return this.props.roomStore.roomFound && (
-      <div style={styles.row}>
-        <div>
-          <ul>
-            {mappedAttendees}
-          </ul>
-        </div>
-        <div>
-          <div>
-            00:00
-          </div>
-          {this.props.roomStore.standingDate !== getToday() &&
-            <Button type="primary" onClick={() => this.standingUp()}>I'M STANDING!</Button>}
+      <Layout style={{minHeight: '100vh'}}>
+        <Header></Header>
+        <Content>
+          <Row type="flex" justify="space-between" align="middle" style={{textAlign: 'center', marginTop:'20px'}}>
+            <Col span={8}>
+              {mappedAttendees}
+            </Col>
+            <Col span={8}>
+              {this.props.roomStore.standingDate !== getToday() &&
+                <Button type="primary" onClick={() => this.standingUp()}>I'M STANDING!</Button>}
 
-          {this.props.roomStore.standingDate === getToday() && this.props.roomStore.sittingDate !== getToday() &&
-            <Button type="danger" size="large" onClick={() => this.sittingDown()}>I'M SITTING!</Button>}
+              {this.props.roomStore.standingDate === getToday() && this.props.roomStore.sittingDate !== getToday() &&
+                <Button type="danger" size="large" onClick={() => this.sittingDown()}>I'M SITTING!</Button>}
 
-          {this.props.roomStore.standingDate === getToday() && this.props.roomStore.sittingDate === getToday() &&
-            <p>Good work today!</p>}
-        </div>
-        <div>
-            {total} Players left
-        </div>
-      </div>
+              {this.props.roomStore.standingDate === getToday() && this.props.roomStore.sittingDate === getToday() &&
+                <p>Good work today!</p>}
+            </Col>
+            <Col span={8}>
+              {total} Attendees left
+            </Col>
+          </Row>
+        </Content>
+        <Footer></Footer>
+      </Layout>
     );
   }
 }
